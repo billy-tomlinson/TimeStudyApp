76c1242d2f9a91ec69da011692b8f47a54833f5a
billy-tomlinson <billytomlinson@gmail.com>
2019-03-10 22:55:10 +0000

    refactors

--- TimeStudyApp/TimeStudyApp/Services/ILocalNotificationService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace TimeStudy.Services
-{
-    public interface ILocalNotificationService
-    {
-        void LocalNotification(string title, string body, int id, DateTime notifyTime, double repeatInterval);
-        void DisableLocalNotification(string title, string body, int id, DateTime notifyTime);
-    }
-}
