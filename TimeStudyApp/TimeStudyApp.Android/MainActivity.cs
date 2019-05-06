15cfdba90b8c46cc3950ae5bc20efdeb27b9d5af
billy-tomlinson <billytomlinson@gmail.com>
2019-05-06 18:09:57 +0100

    added new icon and changed name

--- TimeStudyApp/TimeStudyApp.Android/MainActivity.cs
+++ TimeStudyApp/TimeStudyApp.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using System.IO;
 
 namespace TimeStudyApp.Droid
 {
-    [Activity(Label = "TimeStudyApp", Icon = "@drawable/stopwatch", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Time Study", Icon = "@drawable/timestudy", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
